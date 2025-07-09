83085ab786d0f6ea3e6071f9cd8836095faa3d5c
Kayla <chl324@ucsd.edu>
2025-07-09 19:06:03 +0000

    Implemented UTF-8 analyzer program

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -10,5 +10,21 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // implement the UTF-8 analyzer here
+    char *str = argv[1];
+    char *uppercased = strdup(str);
+    uppercase_ascii(uppercased);
+
+    printf("Valid ASCII: %s\n", is_ascii(str) ? "true" : "false");
+    printf("Uppercased ASCII: %s\n", uppercased);
+    printf("Length in bytes: %zu\n", strlen(str));
+    printf("Number of code points: %d\n", utf8_strlen(str));
+    print_codepoints(str);
+    print_bytes_per_codepoint(str);
+    print_first_6_codepoints(str);
+    print_animal_emojis(str);
+    printf("Next character of Codepoint at index 3: ");
+    print_next_codepoint_at_index_3(str);
+
+    free(uppercased);
+    return 0;
 }
